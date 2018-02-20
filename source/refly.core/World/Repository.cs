4e7c0dab44b3ef16594b50e60872c3b760efffdc
dcornelson <david.cornelson@epsilon.com>
2018-02-20 17:49:24 -0600

    Start working on the Story fliuent language.

---
+++ source/refly.core/World/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refly.core.World
+{
+    public class Repository
+    {
+        private static StoryModel storyData { get; set; } = null;
+        private static List<ItemModel> items { get; set; } = null;
+
+        public static StoryModel StoryData
+        {
+            get
+            {
+                if (storyData == null)
+                {
+                    storyData = new StoryModel();
+                }
+
+                return storyData;
+            }
+            set
+            {
+                storyData = value;
+            }
+        }
+    }
+
+    public class StoryModel
+    {
+        public string Title { get; set; }
+        public string Headline { get; set; }
+    }
+
+    public class ItemModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ source/refly.core/World/Story.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace refly.core.World
+{
+    public class Story
+    {
+        public static string Title
+        {
+            get
+            {
+                return Repository.StoryData.Title;
+            }
+            set
+            {
+                Repository.StoryData.Title = value;
+            }
+        }
+
+        public static string Headline
+        {
+            get
+            {
+                return Repository.StoryData.Headline;
+            }
+            set
+            {
+                Repository.StoryData.Headline = value;
+            }
+        }
+
+        public static Story Create
+        {
+            get
+            {
+                Repository.StoryData = new StoryModel();
+
+                return new Story(); // this is a throwaway instance
+            }
+        }
+    }
+
+    public static class StoryExtensions
+    {
+        public static Story Title(this Story story, string title)
+        {
+            story.Title(title);
+            return story;
+        }
+
+        public static Story Headline(this Story story, string headline)
+        {
+            story.Headline(headline);
+            return story;
+        }
+    }
+
+    public class StoryTest
+    {
+        public StoryTest()
+        {
+            Story.Create
+                .Title("Title")
+                .Headline("Headline");
+
+            Console.WriteLine(Story.Title);
+        }
+    }
+
+}
